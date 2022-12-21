bfe30fdabfb2e57f43b3223fae150a2b6fb17eb8
Bruna Terribile <brunaterribile@gmail.com>
2022-12-21 12:58:38 -0300

    Refact: signIn/signUp + signIn function +  uuid

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,7 +1,8 @@
 import connectionDB from "../database/db.js";
 import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
 
-export async function SignIn(req, res){
+export async function SignUp(req, res){
     const {name, email, password, createdAt} = req.body
 
     try{
@@ -18,6 +19,25 @@ export async function SignIn(req, res){
     }
 }
 
-export async function SignUp(req, res){
-    
+export async function SignIn(req, res){
+    const {email, password} = req.body;
+    const token = uuidV4();
+
+    try {
+        const userExist = await connectionDB.query(`
+            SELECT * FROM users WHERE email = $1`,[email]); //verifica se o email está cadastrado do bd
+        if(userExist.rowCount == 0) {
+            return res.status(401).send("Email não encontrado")
+        }
+        
+        const passwordOk = bcrypt.compareSync(password, userExist.rows[0].password); //verifica se a senha foi preenchida corretamente
+        if(!passwordOk) {
+            return res.sendStatus(401)
+        }
+
+    res.send({ token })
+    } catch (err) {
+        console.log(err)
+        res.sendStatus(500)
+    }
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,7 @@ import { SignIn, SignUp } from "../controllers/users.controllers.js";
 
 const router = Router();
 
-router.post("/signin", SignIn);
 router.post("/signup", SignUp);
+router.post("/signin", SignIn);
 
 export default router;
