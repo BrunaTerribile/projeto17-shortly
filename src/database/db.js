4d42672e24228eaf623be0a8cb16127fed357d03
Bruna Terribile <brunaterribile@gmail.com>
2022-12-21 10:44:14 -0300

    DB connection + install joi

--- src/database/db.js
+++ src/database/db.js
@@ -1,12 +1,12 @@
-import pg from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { Pool } = pg;
+const { Pool } = pkg;
 
 export const connectionDB = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: true
 })
 
 export default connectionDB;
