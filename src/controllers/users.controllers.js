6ebdddd9516627995fc8a29bca67bbbdf8779648
Bruna Terribile <brunaterribile@gmail.com>
2022-12-21 12:45:46 -0300

    Feat: hash user password + install bcrypt

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,13 +1,16 @@
 import connectionDB from "../database/db.js";
+import bcrypt from "bcrypt";
 
 export async function SignIn(req, res){
     const {name, email, password, createdAt} = req.body
 
     try{
+        const hashPassword = bcrypt.hashSync(password, 10) //criptografa a senha do usuário
+
         const result = await connectionDB.query(`
             INSERT INTO users (name, email, password, "createdAt")
             VALUES ($1, $2, $3, $4)`,
-            [name, email, password, createdAt]);
+            [name, email, hashPassword, createdAt]);
         res.status(201).send(result)
     } catch(err){
         console.log(err);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,7 @@
 import joi from 'joi';
 
 export const userSchema = joi.object({
-    name: joi.string().required(),
-    email: joi.email().required(),
-    password: joi.password().required(),
-    confirmPassword: joi.string()
+    name: joi.string().required().min(3),
+    email: joi.string().email().required().min(3),
+    password: joi.string().required()
 })
